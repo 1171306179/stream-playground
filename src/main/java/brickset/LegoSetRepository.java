514cf4ddb895259080ac9724cbe3963d55aaef52
Zhanghaoyang <1171306179@qq.com>
2022-04-09 00:11:21 +0200

    Working with Stream2

--- src/main/java/brickset/LegoSetRepository.java
+++ src/main/java/brickset/LegoSetRepository.java
@@ -2,7 +2,14 @@ package brickset;
 
 import repository.Repository;
 
+import java.util.Comparator;
+import java.util.Map;
 import java.util.Objects;
+import java.util.stream.Collector;
+import java.util.stream.Collectors;
+
+import static java.util.stream.Collectors.counting;
+import static java.util.stream.Collectors.groupingBy;
 
 /**
  * Represents a repository of {@code LegoSet} objects.
@@ -72,11 +79,76 @@ public class LegoSetRepository extends Repository<LegoSet> {
      * @return String
      */
     public void YearOfLegoSets(){
-        (getAll().stream().
-                map(LegoSet::getYear)).
+        getAll().stream().
+                map(LegoSet::getYear).
                 limit(10).
                 forEach(System.out::println);
     }
+
+    //++++++++++++++++++++++++++++++++++++++
+
+    /**
+     * Method 1
+     * Return True or False for LegoSets which pieces equals 100.
+     *
+     * @return boolean
+     */
+    public boolean MatchPiece(){
+        return getAll().stream().
+                anyMatch(LegoSet -> LegoSet.getPieces() == 100);
+    }
+
+    /**
+     * Method 2
+     * Return Tags of LegoSets which is not null.
+     *
+     * @return String
+     */
+    public void MapTags(){
+        getAll().stream().
+                filter(LegoSet->LegoSet.getTags() != null).
+                flatMap(LegoSet -> LegoSet.getTags().stream()).
+                distinct().
+                forEach(System.out::println);
+    }
+
+    /**
+     * Method 3
+     * Return the Total pieces.
+     *
+     * @return value the value of Total pieces
+     */
+    public long TotalPiece(){
+        return getAll().stream().
+                map(LegoSet::getPieces).
+                reduce(0,(a,b)->a+b);
+    }
+
+
+    /**
+     * Method 4
+     * Return the number of each LegoSet Theme.
+     *
+     * @return Map each LegoSet Theme and the number of each Theme
+     */
+    public Map<String,Long> NumberByTheme() {
+        return getAll().stream().collect(groupingBy(LegoSet::getTheme, counting()));
+
+    }
+
+
+    /**
+     * Method 5
+     * Return the number of each LegoSet Subtheme.
+     *
+     * @return Map each LegoSet Subtheme and the number of each Subtheme
+     */
+    public Map<String,Long> NumberBySubTheme(){
+        return getAll().stream().
+                filter(LegoSet->LegoSet.getSubtheme() != null).
+                collect(Collectors.groupingBy(LegoSet::getSubtheme,Collectors.counting()));
+    }
+
 
 
     public static void main(String[] args) {
@@ -103,8 +175,30 @@ public class LegoSetRepository extends Repository<LegoSet> {
         repository.YearOfLegoSets();
         System.out.println("==========================");
 
+        System.out.println();
+        System.out.println("+++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++");
+        System.out.println();
 
 
+       //method 1
+        System.out.println(repository.MatchPiece());
+        System.out.println("===========================");
+
+        //method 2
+        repository.MapTags();
+        System.out.println("===========================");
+
+        //method 3
+        System.out.println(repository.TotalPiece());
+        System.out.println("===========================");
+
+        //method 4
+        System.out.println(repository.NumberByTheme());
+        System.out.println("===========================");
+
+        //method 5
+        System.out.println(repository.NumberBySubTheme());
+        System.out.println("===========================");
     }
 
 }
